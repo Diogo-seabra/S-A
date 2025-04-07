7bb933ac216da2baa65f635e27bc140ca39cb697
Diogo Seabra <seabradiogo33@gmail.com>
2025-04-07 17:10:44 -0300

    Criando a seção do carrinho

---
+++ src/components/Cart/CartOverlay.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+const CartOverlay = () => {
+    const [isCartOpen, setIsCartOpen] = useState(true);
+
+    return (
+        <div className={`h-screen w-screen fixed top-0 left-0 z-50`}>
+            <section
+                id='outside-of-cart'
+                onClick={() => setIsCartOpen(false)}
+                className={`absolute top-0 left-0 w-full h-full bg-slate-950 transition-opacity duration-500 ${
+                    isCartOpen ? 'opacity-50' : 'opacity-0 pointer-events-none'
+                }`}
+            ></section>
+            <section
+                id='inside-of-cart'
+                className={`absolute top-0 right-0 h-full bg-slate-950 min-w-96 w-1/4 border-l-4 border-slate-400 p-5 flex flex-col justify-between text-white transform transition-transform duration-500 ${
+                    isCartOpen ? 'translate-x-0' : 'translate-x-full'
+                }`}
+            ></section>
+        </div>
+    );
+};
+
+export default CartOverlay;
