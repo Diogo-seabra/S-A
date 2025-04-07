e7c5a879336dc60fa9ca0f159bde647621f0c6fe
Diogo Seabra <seabradiogo33@gmail.com>
2025-04-07 20:21:15 -0300

    Substituir as âncoras pelo componente Link do react-router-dom

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,31 @@
 import logo from '../assets/logo/logo.svg';
 import UserButtons from './UserButtons';
-import { useLocation } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
 
 const Header = () => {
     const { pathname } = useLocation();
     const completeHeader = (
         <header className='flex text-xl sticky top-0 shadow-xl shadow-slate-400 bg-slate-950 text-slate-200 px-8 py-4 items-end justify-between z-10'>
-            <a href='/'>
+            <Link to='/'>
                 <img
                     className='px-2 w-26 h-auto'
                     src={logo}
                     alt='Logo da S&A'
                 />
-            </a>
+            </Link>
             <UserButtons />
         </header>
     );
 
     const simpleHeader = (
         <header className='flex text-xl sticky top-0 shadow-xl shadow-slate-400 bg-slate-950 text-slate-200 px-8 py-4 items-end justify-between z-10'>
-            <a href='/'>
+            <Link to='/'>
                 <img
                     className='px-2 w-20 h-auto'
                     src={logo}
                     alt='Logo da S&A'
                 />
-            </a>
+            </Link>
         </header>
     );
 
--- src/components/UserButtons.jsx
+++ src/components/UserButtons.jsx
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartShopping, faUser } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 const UserButtons = () => {
     return (
@@ -13,9 +14,9 @@ const UserButtons = () => {
                     3
                 </div>
             </button>
-            <a className='px-2' href='#'>
+            <Link to='/history' className='px-2'>
                 <FontAwesomeIcon icon={faUser} className='text-2xl' />
-            </a>
+            </Link>
         </div>
     );
 };
