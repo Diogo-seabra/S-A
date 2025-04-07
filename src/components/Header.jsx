8543b332e23e69ea46365ca43b943f449b69924c
Diogo Seabra <seabradiogo33@gmail.com>
2025-04-07 20:14:24 -0300

    Criar header dinâmico que reage de acordo com a página atual

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
 import logo from '../assets/logo/logo.svg';
 import UserButtons from './UserButtons';
+import { useLocation } from 'react-router-dom';
 
 const Header = () => {
-    return (
+    const { pathname } = useLocation();
+    const completeHeader = (
         <header className='flex text-xl sticky top-0 shadow-xl shadow-slate-400 bg-slate-950 text-slate-200 px-8 py-4 items-end justify-between z-10'>
             <a href='/'>
                 <img
-                    className='h-16 px-2 w-25 h-auto'
+                    className='px-2 w-26 h-auto'
                     src={logo}
                     alt='Logo da S&A'
                 />
@@ -14,6 +16,20 @@ const Header = () => {
             <UserButtons />
         </header>
     );
+
+    const simpleHeader = (
+        <header className='flex text-xl sticky top-0 shadow-xl shadow-slate-400 bg-slate-950 text-slate-200 px-8 py-4 items-end justify-between z-10'>
+            <a href='/'>
+                <img
+                    className='px-2 w-20 h-auto'
+                    src={logo}
+                    alt='Logo da S&A'
+                />
+            </a>
+        </header>
+    );
+
+    return <>{pathname === '/' ? completeHeader : simpleHeader}</>;
 };
 
 export default Header;
