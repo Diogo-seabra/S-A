079f8b132cc83143ec681c1eb9eb5e688172f9bc
Diogo Seabra <seabradiogo33@gmail.com>
2025-04-08 15:37:24 -0300

    Gerar catálogo indexado por ID a partir do array

--- src/components/Cart/CartItem.jsx
+++ src/components/Cart/CartItem.jsx
@@ -1,7 +1,9 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrashCan, faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
+import { catalogIndexedById } from '../../utils/catalog';
 
 const CartItem = () => {
+    console.log(catalogIndexedById);
     return (
         <article className='flex bg-stone-100 p-1 border rounded-mg relative'>
             <img src='' alt='' className='h-24' />
@@ -14,11 +16,11 @@ const CartItem = () => {
                 <p className='text-green-700 text-lg'>Preço</p>
                 <div className='flex text-slate-950 absolute right-2 bottom-0 mb-2'>
                     <button className='border border-slate-400 hover:border-slate-900 mb-2'>
-                        <FontAwesomeIcon icon={faMinus} className='p-1'/>
+                        <FontAwesomeIcon icon={faMinus} className='p-1' />
                     </button>
                     <p className='p-2 w-8 mx-auto'>2</p>
                     <button className='border border-slate-400 hover:border-slate-900 mb-2'>
-                        <FontAwesomeIcon icon={faPlus} className='p-1'/>
+                        <FontAwesomeIcon icon={faPlus} className='p-1' />
                     </button>
                 </div>
             </div>
