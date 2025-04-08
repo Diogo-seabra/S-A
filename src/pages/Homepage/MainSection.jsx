c90bef9783c70e8895e7f3a2295f53882d96bee8
Diogo Seabra <seabradiogo33@gmail.com>
2025-04-08 19:26:49 -0300

    Implementar a filtragem por gênero

--- src/pages/Homepage/MainSection.jsx
+++ src/pages/Homepage/MainSection.jsx
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import ProductFilters from "./ProductFilters";
 import ProductsContainer from "./ProductsContainer";
 
 const MainSection = () => {
+    const [femaleProducts, setFemaleProducts] = useState(null)
+
     return (
         <>
-            <ProductFilters />
-            <ProductsContainer />
+            <ProductFilters setFemaleProducts={setFemaleProducts}/>
+            <ProductsContainer femaleProducts={femaleProducts}/>
         </>
     );
 };
--- src/pages/Homepage/ProductsContainer.jsx
+++ src/pages/Homepage/ProductsContainer.jsx
@@ -1,11 +1,14 @@
 import { catalog } from '../../utils/catalog';
 import ProductCard from './ProductCard';
 
-const ProductsContainer = () => {
+const ProductsContainer = ({ femaleProducts }) => {
     return (
         <section className='container flex flex-wrap mx-auto p-10 justify-center gap-10'>
-            {catalog.map((product) => (
-                <ProductCard key={`product_${product.id}_key`} {...product}/>
+            {(femaleProducts !== null
+                ? catalog.filter((p) => p.feminine === femaleProducts)
+                : catalog
+            ).map((product) => (
+                <ProductCard key={`product_${product.id}_key`} {...product} />
             ))}
         </section>
     );
