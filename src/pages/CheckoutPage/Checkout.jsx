427f410932534acdcd3e1ba0cd36972fdae2edb6
Diogo Seabra <seabradiogo33@gmail.com>
2025-04-09 16:42:21 -0300

    Criar componente genérico de formulário

--- src/pages/CheckoutPage/Checkout.jsx
+++ src/pages/CheckoutPage/Checkout.jsx
@@ -1,3 +1,23 @@
-const Checkout = () => {};
+import FormInput from '../../utils/FormInput';
+
+const Checkout = () => {
+    return (
+        <main className='bg-stone-200 h-[calc(100dvh-6rem)]'>
+            <p className='text-center text-2xl font-bold text-slate-950  pt-8'>
+                Finalizar Compra
+            </p>
+            <form className='grid grid-rows-5 grid-cols-3 grid-flow-col gap-4 h-3/4 mt-6 px-8'>
+                <FormInput
+                    fieldType={'text'}
+                    fieldName={'Nome'}
+                    id='Nome'
+                    placeholder={'John'}
+                    className=''
+                    required
+                />
+            </form>
+        </main>
+    );
+};
 
 export default Checkout;
