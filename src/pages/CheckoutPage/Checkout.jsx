f4224daf0fe9248a4619667d5e96979cdd429f70
Diogo Seabra <seabradiogo33@gmail.com>
2025-04-09 17:03:08 -0300

    Adaptar o grid para os elementos

--- src/pages/CheckoutPage/Checkout.jsx
+++ src/pages/CheckoutPage/Checkout.jsx
@@ -6,7 +6,10 @@ const Checkout = () => {
             <p className='text-center text-2xl font-bold text-slate-950  pt-8'>
                 Finalizar Compra
             </p>
-            <form className='grid grid-rows-5 grid-cols-3 grid-flow-col gap-4 h-3/4 mt-6 px-8'>
+            <form className='grid grid-rows-[max-content_1fr_1fr_1fr_1fr_1fr] grid-cols-3 grid-flow-col gap-4 h-3/4 mt-6 px-8'>
+                <p className='text-center text-sm font-bold text-slate-950'>
+                    Seus dados
+                </p>
                 <FormInput
                     fieldType={'text'}
                     fieldName={'Nome'}
@@ -15,6 +18,16 @@ const Checkout = () => {
                     className=''
                     required
                 />
+
+                <p className='text-center text-sm font-bold text-slate-950 row-start-1'>
+                    Pagamento e Entrega
+                </p>
+        
+
+                <p className='text-center text-sm font-bold text-slate-950 row-start-1'>
+                    Seus Produtos
+                </p>
+                
             </form>
         </main>
     );
