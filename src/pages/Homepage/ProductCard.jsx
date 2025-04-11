96c70769746327e30059c39f8e8ecd083758c50a
Diogo Seabra <seabradiogo33@gmail.com>
2025-04-11 13:48:12 -0300

    Adicionar responsividade na header e no carrinho

--- src/pages/Homepage/ProductCard.jsx
+++ src/pages/Homepage/ProductCard.jsx
@@ -7,28 +7,32 @@ const ProductCard = ({ id, name, brand, image, feminine, price }) => {
 
     return (
         <>
-            <article className='w-1/3 md:w-1/6 lg:w-1/8 relative card-produto group bg-stone-100 shadow-md shadow-slate-200 flex flex-col justify-between border border-gray-200 rounded-xl p-4 transition-all hover:scale-105'>
+            <article className='w-1/3 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 2xl:w-1/6 max-w-[200px] min-w-[150px] relative card-produto group bg-stone-100 shadow-md shadow-slate-200 flex flex-col justify-between border border-gray-200 rounded-xl p-4 transition-all hover:scale-105'>
+
                 <img
                     className='group-hover:scale-105 rounded-lg transition duration-300 mb-4'
                     src={image}
                     alt={`Imagem do produto ${id}`}
                 />
 
-                <div className='flex flex-col gap-1 mb-8'>
+                <div className='flex flex-col gap-1'>
                     <p className='text-sm text-slate-400'>{brand}</p>
                     <p className='text-sm '>{name}</p>
-                    <p className='text-base font-medium text-green-700 absolute bottom-12.5 left-4'>
-                        R${price}
-                    </p>
                 </div>
 
-                <button
-                    onClick={() => addToCart(id)}
-                    className='bg-slate-900 rounded-md text-slate-100 hover:bg-slate-800 transition duration-300 flex items-center justify-center gap-2 py-1 px-4 w-full cursor-pointer'
-                >
-                    Adicionar
-                    <FontAwesomeIcon icon={faCartPlus} />
-                </button>
+
+                <div>
+                    <p className='text-base font-medium text-green-700 pt-2 pb-2'>
+                        R${price}
+                    </p>
+                    <button
+                        onClick={() => addToCart(id)}
+                        className='text-sm bg-slate-900 rounded-md text-slate-100 hover:bg-slate-800 transition duration-300 flex items-center justify-center gap-1 py-1 px-4 w-full cursor-pointer'
+                    >
+                        Adicionar
+                        <FontAwesomeIcon icon={faCartPlus} />
+                    </button>
+                </div>
             </article>
         </>
     );
--- src/pages/Homepage/ProductsContainer.jsx
+++ src/pages/Homepage/ProductsContainer.jsx
@@ -3,9 +3,13 @@ import ProductCard from './ProductCard';
 
 const ProductsContainer = ({ searchParams }) => {
     return (
-        <section className='container flex flex-wrap mx-auto p-10 justify-center gap-10 md:flex-row'>
+        <section className='flex flex-wrap justify-center gap-8 sm:gap-6 px-1 sm:px-4 md:px-10 lg:px-16'>
             {(searchParams.get('filterby') !== null
-                ? catalog.filter((p) => p.feminine === (searchParams.get('filterby') === 'fem'))
+                ? catalog.filter(
+                      (p) =>
+                          p.feminine ===
+                          (searchParams.get('filterby') === 'fem')
+                  )
                 : catalog
             ).map((product) => (
                 <ProductCard key={`product_${product.id}_key`} {...product} />
@@ -15,4 +19,3 @@ const ProductsContainer = ({ searchParams }) => {
 };
 
 export default ProductsContainer;
-
